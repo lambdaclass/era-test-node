a36c165ddfeed2f93dc8eb40dda3ce1d8648431d
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2023-11-27 08:19:40 -0300

    update zksolc version

--- etc/system-contracts/scripts/compile-yul.ts
+++ etc/system-contracts/scripts/compile-yul.ts
@@ -4,7 +4,7 @@ import { exec as _exec, spawn as _spawn } from 'child_process';
 
 import { getZksolcUrl, saltFromUrl } from '@matterlabs/hardhat-zksync-solc';
 
-const COMPILER_VERSION = '1.3.14';
+const COMPILER_VERSION = '1.3.17';
 const IS_COMPILER_PRE_RELEASE = false;
 
 import path from "path";
