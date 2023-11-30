7cec056cd306dd0c72210bef7a58a11ae7cd6ecb
Iván Litteri <ilitteri@fi.uba.ar>
2023-11-30 14:34:54 -0300

    Implement ERC20 for L2EthToken

--- etc/system-contracts/contracts/L2EthToken.sol
+++ etc/system-contracts/contracts/L2EthToken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import {IEthToken} from "./interfaces/IEthToken.sol";
+import {ERC20} from "./openzeppelin/token/ERC20/ERC20.sol";
 import {MSG_VALUE_SYSTEM_CONTRACT, DEPLOYER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS, L1_MESSENGER_CONTRACT} from "./Constants.sol";
 import {SystemContractHelper} from "./libraries/SystemContractHelper.sol";
 import {IMailbox} from "./interfaces/IMailbox.sol";
@@ -14,16 +14,27 @@ import {IMailbox} from "./interfaces/IMailbox.sol";
  * Instead, this contract is used by the bootloader and `MsgValueSimulator`/`ContractDeployer` system contracts
  * to perform the balance changes while simulating the `msg.value` Ethereum behavior.
  */
-contract L2EthToken is IEthToken {
-    /// @notice The balances of the users.
-    mapping(address => uint256) balance;
-
+contract L2EthToken is ERC20 {
     /// @notice The total amount of tokens that have been minted.
-    uint256 public override totalSupply;
+    uint256 public _totalSupply;
 
     modifier onlyBootloader() {
         require(msg.sender == BOOTLOADER_FORMAL_ADDRESS, "Callable only by the bootloader");
         _;
+    }
+
+    event Mint(address indexed account, uint256 amount);
+
+    event Withdrawal(address indexed _l2Sender, address indexed _l1Receiver, uint256 _amount);
+
+    event WithdrawalWithMessage(
+        address indexed _l2Sender,
+        address indexed _l1Receiver,
+        uint256 _amount,
+        bytes _additionalData
+    );
+
+    constructor() ERC20(name(), symbol()) {
     }
 
     /// @notice Transfer tokens from one address to another.
@@ -33,7 +44,7 @@ contract L2EthToken is IEthToken {
     /// @dev This function can be called only by trusted system contracts.
     /// @dev This function also emits "Transfer" event, which might be removed
     /// later on.
-    function transferFromTo(address _from, address _to, uint256 _amount) external override {
+    function transferFromTo(address _from, address _to, uint256 _amount) external {
         require(
             msg.sender == MSG_VALUE_SYSTEM_CONTRACT ||
                 msg.sender == address(DEPLOYER_SYSTEM_CONTRACT) ||
@@ -41,39 +52,26 @@ contract L2EthToken is IEthToken {
             "Only system contracts with special access can call this method"
         );
 
-        uint256 fromBalance = balance[_from];
+        uint256 fromBalance = balanceOf(_from);
         require(fromBalance >= _amount, "Transfer amount exceeds balance");
-        unchecked {
-            balance[_from] = fromBalance - _amount;
-            // Overflow not possible: the sum of all balances is capped by totalSupply, and the sum is preserved by
-            // decrementing then incrementing.
-            balance[_to] += _amount;
-        }
+        transferFrom(_from, _to, _amount);
 
         emit Transfer(_from, _to, _amount);
-    }
-
-    /// @notice Returns ETH balance of an account
-    /// @dev It takes `uint256` as an argument to be able to properly simulate the behaviour of the
-    /// Ethereum's `BALANCE` opcode that accepts uint256 as an argument and truncates any upper bits
-    /// @param _account The address of the account to return the balance of.
-    function balanceOf(uint256 _account) external view override returns (uint256) {
-        return balance[address(uint160(_account))];
     }
 
     /// @notice Increase the total supply of tokens and balance of the receiver.
     /// @dev This method is only callable by the bootloader.
     /// @param _account The address which to mint the funds to.
     /// @param _amount The amount of ETH in wei to be minted.
-    function mint(address _account, uint256 _amount) external override onlyBootloader {
-        totalSupply += _amount;
-        balance[_account] += _amount;
+    function mint(address _account, uint256 _amount) external onlyBootloader {
+        _totalSupply += _amount;
+        _mint(_account, _amount);
         emit Mint(_account, _amount);
     }
 
     /// @notice Initiate the ETH withdrawal, funds will be available to claim on L1 `finalizeEthWithdrawal` method.
     /// @param _l1Receiver The address on L1 to receive the funds.
-    function withdraw(address _l1Receiver) external payable override {
+    function withdraw(address _l1Receiver) external payable {
         uint256 amount = _burnMsgValue();
 
         // Send the L2 log, a user could use it as proof of the withdrawal
@@ -86,7 +84,7 @@ contract L2EthToken is IEthToken {
     /// @notice Initiate the ETH withdrawal, with the sent message. The funds will be available to claim on L1 `finalizeEthWithdrawal` method.
     /// @param _l1Receiver The address on L1 to receive the funds.
     /// @param _additionalData Additional data to be sent to L1 with the withdrawal.
-    function withdrawWithMessage(address _l1Receiver, bytes memory _additionalData) external payable override {
+    function withdrawWithMessage(address _l1Receiver, bytes memory _additionalData) external payable {
         uint256 amount = _burnMsgValue();
 
         // Send the L2 log, a user could use it as proof of the withdrawal
@@ -107,8 +105,8 @@ contract L2EthToken is IEthToken {
         unchecked {
             // This is safe, since this contract holds the ether balances, and if user
             // send a `msg.value` it will be added to the contract (`this`) balance.
-            balance[address(this)] -= amount;
-            totalSupply -= amount;
+            _burn(address(this), amount);
+            _totalSupply -= amount;
         }
     }
 
@@ -129,19 +127,19 @@ contract L2EthToken is IEthToken {
 
     /// @dev This method has not been stabilized and might be
     /// removed later on.
-    function name() external pure override returns (string memory) {
-        return "Ether";
+    function name() public view virtual override returns (string memory) {
+        return "Lambda";
     }
 
     /// @dev This method has not been stabilized and might be
     /// removed later on.
-    function symbol() external pure override returns (string memory) {
-        return "ETH";
+    function symbol() public view virtual override returns (string memory) {
+        return "LBD";
     }
 
     /// @dev This method has not been stabilized and might be
     /// removed later on.
-    function decimals() external pure override returns (uint8) {
+    function decimals() public view virtual override returns (uint8) {
         return 18;
     }
 }
